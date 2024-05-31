bce40344d214deb081087b6d8e0a837f186ade52
Serotina <wlsdn2749@gmail.com>
2024-05-31 12:22:21 +0900

    Update : 38_Count_And_Say.py

---
+++ LeetCode/38_Count_And_Say.py
@@ -0,0 +1,37 @@
+class Solution:
+    
+    def __init__(self):
+        self.rle = ["1", "1"]
+        self.func()
+        
+    def countAndSay(self, n: int) -> str:
+        return self.rle[n]
+    
+    def func(self):
+        
+        for i in range(30):
+            base = self.rle[-1]
+            cur = 0
+            count = 0
+            temp = ""
+            while base:
+                if cur == 0:
+                    cur = base[0]
+                    count += 1
+                elif cur == base[0]:
+                    count += 1
+                    
+                else:
+                    temp += f"{count}{cur}"
+                    cur = base[0]
+                    count = 1 
+                    
+                base = base[1:]
+            
+            temp += f"{count}{cur}"
+            self.rle.append(temp)
+            
+        
+        
+s = Solution()
+print( s.countAndSay(4) )
