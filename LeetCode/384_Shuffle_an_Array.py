32fcb6a827787ee44ad26f345c3d863ed00af841
Serotina <wlsdn2749@gmail.com>
2024-07-31 16:26:06 +0900

    Update : 384_Shuffle_an_Array.py

---
+++ LeetCode/384_Shuffle_an_Array.py
@@ -0,0 +1,24 @@
+from typing import List
+
+import random
+
+class Solution:
+
+    def __init__(self, nums: List[int]):
+        self.nums = nums
+
+    def reset(self) -> List[int]:
+        return self.nums
+
+    def shuffle(self) -> List[int]:
+        indexes = list(range(len(self.nums)))
+        random.shuffle(indexes)
+        return [self.nums[i] for i in indexes]
+        
+        
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.reset()
+# param_2 = obj.shuffle()
