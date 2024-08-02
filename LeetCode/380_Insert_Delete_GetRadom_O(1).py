d00eab9528fdbe5032d555d30cf976a7aa1d005f
Serotina <wlsdn2749@gmail.com>
2024-08-02 14:08:16 +0900

    Update : 1152 1152.c

--- LeetCode/380_Insert_Delete_GetRadom_O(1).py
+++ LeetCode/380_Insert_Delete_GetRadom_O(1).py
@@ -1,23 +1,36 @@
+import random
+
 class RandomizedSet:
 
     def __init__(self):
-        self.randomized_set = {}
+        self.a, self.d = [], {}
+
             
     def insert(self, val: int) -> bool:
-        if val in self.randomized_set:
-            return False
-        else:
-            self.randomized_set.add(val)
+        if val not in self.randomized_set:
+            self.d[val] = len(self.a)
+            self.a.append(val)
+            
             return True
-        
 
     def remove(self, val: int) -> bool:
         
-        if val in self.randomized_set:
-            self.randomized_set.remove(val)
+        if val in self.d:
+            e, i = self.a.pop(), self.d.pop(val)
+            if i < len(self.a):
+                self.a[i], self.d[e] = e, i
+
             return True
-        
-        return False 
 
     def getRandom(self) -> int:
-        
+        return self.choice(self.a)
+    
+
+randomizedSet = RandomizedSet()
+print(randomizedSet.insert(1))
+print(randomizedSet.insert(2))
+print(randomizedSet.remove(2))
+print(randomizedSet.getRandom())
+print(randomizedSet.remove(1))
+print(randomizedSet.insert(2))
+print(randomizedSet.getRandom())
